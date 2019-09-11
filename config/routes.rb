f178edc44edcad3d89bc744b9724863a0c4e23dc
Richard Pitts <Richardepitts@gmail.com>
2019-09-11 09:58:19 -0500

    -

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'topics#index'
+  get 'topics', to: 'topics#index'
   get 'html', to: 'topics#html'
   get 'javascript', to: 'topics#javascript'
   get 'css', to: 'topics#css'
